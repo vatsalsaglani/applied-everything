889df3959ecc9c62d0bf83a44af05ef4486cb66a
vatsals <vatsals@quinnox.com>
2021-05-23 11:36:20 +0530

    added adsense

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,13 @@ export default class MyDocument extends NextDocument {
         {/* <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" /> */}
         {/* <Head /> */}
         <Head>
+            {/* adsense */}
+          <script
+            data-ad-client="ca-pub-9723474343210663"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+          {/* analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
