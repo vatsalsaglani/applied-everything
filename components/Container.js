fa2bec656d59e64fbb56baf284783681fe3d258e
vatsals <vatsals@quinnox.com>
2021-07-22 21:23:32 +0530

    removed unnecessary code

--- components/Container.js
+++ components/Container.js
@@ -1,85 +1,51 @@
-import React from 'react'
-import {
-    useColorMode,
-    Button,
-    Flex,
-    Box
-} from '@chakra-ui/react'
-import NextLink from 'next/link'
-import styled from '@emotion/styled'
+import React from "react";
+import { useColorMode, Button, Flex, Box } from "@chakra-ui/react";
+import NextLink from "next/link";
+import styled from "@emotion/styled";
 
 // import DarkModeSwitch from '../components/DarkModeSwitch'
 
 const Container = ({ children }) => {
-    const { colorMode } = useColorMode()
+  const { colorMode } = useColorMode();
 
-    const bgColor = {
-        light: 'whiteAlpha.400',
-        dark: '#1A212C'
-    }
+  const bgColor = {
+    light: "whiteAlpha.400",
+    dark: "#1A212C",
+  };
 
-    const color = {
-        light: 'black',
-        dark: 'white'
-    }
+  const color = {
+    light: "black",
+    dark: "white",
+  };
 
-    const navHoverBg = {
-        light: 'gray.600',
-        dark: 'gray.300',
-    }
+  const navHoverBg = {
+    light: "gray.600",
+    dark: "gray.300",
+  };
 
-    const StickyNav = styled(Flex)`
-        position: sticky;
-        z-index: 10;
-        top: 0;
-        backdrop-filter: saturate(180%) blur(20px);
-        transition: height .5s, line-height .5s;
-        `
+  const StickyNav = styled(Flex)`
+    position: sticky;
+    z-index: 10;
+    top: 0;
+    backdrop-filter: saturate(180%) blur(20px);
+    transition: height 0.5s, line-height 0.5s;
+  `;
 
-    return (
-        <>
-            {/* <StickyNav
-                flexDirection="row"
-                justifyContent="space-between"
-                alignItems="center"
-                maxWidth="800px"
-                minWidth="356px"
-                width="100%"
-                bg={bgColor[colorMode]}
-                as="nav"
-                px={[2, 6, 6]}
-                py={2}
-                mt={8}
-                mb={[0, 0, 8]}
-                mx="auto"
-            >
-                <Box>
-                    <NextLink href="/" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
-                            Home
-                        </Button>
-                    </NextLink>
-                    <NextLink href="/blog" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
-                            Blog
-                        </Button>
-                    </NextLink>
-                </Box>
-                <DarkModeSwitch />
-            </StickyNav > */}
-            <Flex
-                // as="main"
-                justifyContent="center"
-                flexDirection="column"
-                bg={bgColor[colorMode]}
-                color={color[colorMode]}
-                // px={[0, 4, 4]}
-                // mt={[4, 8, 8]}
-            >
-                {children}
-            </Flex>
-        </>
-    )
-}
+  return (
+    <>
+      <Flex
+        // as="main"
+        justifyContent="center"
+        flexDirection="column"
+        bg={bgColor[colorMode]}
+        color={color[colorMode]}
+        // px={[0, 4, 4]}
+        // mt={[4, 8, 8]}
+      >
+        {children}
+      </Flex>
+    </>
+  );
+};
 
-export default Container
+export default Container;
