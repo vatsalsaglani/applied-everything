4ed186841735335514822d2f3e364563aa0d5e4d
vatsals <vatsals@quinnox.com>
2021-07-22 18:23:07 +0530

    added blog card

--- components/ListBlogs.js
+++ components/ListBlogs.js
@@ -8,6 +8,10 @@ import {
   Tag,
   SimpleGrid,
   Text,
+  Center,
+  Image,
+  Stack,
+  useColorModeValue,
 } from "@chakra-ui/react";
 
 import { parseISO, format } from "date-fns";
@@ -34,7 +38,70 @@ export const ListTags = ({ tags }) => {
   });
 };
 
-const ListBlogs = ({ title, tags, summary, slug, reading_time }) => {
+const RenderBlogCard = ({
+  cover_image,
+  title,
+  tags,
+  summary,
+  slug,
+  reading_time,
+}) => (
+  <Link href={`/blog/${slug}`}>
+    <a>
+
+    <Center py={6}>
+      <Box
+        maxW={"445px"}
+        w={"full"}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        p={6}
+        overflow={"hidden"}
+      >
+        <Box
+          // h={"210px"}
+          bg={"gray.100"}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={"relative"}
+        >
+          <Image src={cover_image} layout={"cover"} />
+        </Box>
+        <Stack>
+          <Text
+            color={"green.500"}
+            textTransform={"uppercase"}
+            fontWeight={800}
+            fontSize={"sm"}
+            letterSpacing={1.1}
+          >
+            {reading_time.text}
+          </Text>
+          <Heading
+            color={useColorModeValue("gray.700", "white")}
+            fontSize={"2xl"}
+            fontFamily={"body"}
+          >
+            {title}
+          </Heading>
+          <Text color={"gray.500"}>{summary}</Text>
+        </Stack>
+      </Box>
+    </Center>
+    </a>
+  </Link>
+);
+
+const ListBlogs = ({
+  title,
+  tags,
+  summary,
+  slug,
+  reading_time,
+  cover_image,
+}) => {
   const { colorMode } = useColorMode();
   // const listTags = tags.split(", ");
   // console.log("Tags -- ", listTags)
@@ -51,84 +118,92 @@ const ListBlogs = ({ title, tags, summary, slug, reading_time }) => {
     light: "black",
   };
 
+  console.log(title, tags, summary, slug, reading_time, cover_image);
+
   return (
     // <NextLink href={`/blog/${slug}`} passHref>
-
-    <Container
-      mt="50px"
-      mb="15px"
-      // boxShadow="inset -30px -30px 29px #110e27,             inset 30px 30px 29px #151231;"
-      borderRadius="21px"
-      display="flex"
-      flexDirection="column"
-      alignItems="space-around"
-      justifyContent="center"
-      border={`2px solid ${borderColor[colorMode]}`}
-      minWidth="100%"
-    >
-      <Box>
-        <Link href={`/blog/${slug}`}>
-          <a>
-            <Text
-              fontWeight="bold"
-              textAlign="justify"
-              color={textColor[colorMode]}
-              fontSize="xl"
-              letterSpacing="wide"
-              mt="15px"
-              mb="1px"
-              ml="4px"
-            >
-              {/* Title for the Blog */}
-              {title}
-            </Text>
-          </a>
-        </Link>
-      </Box>
-      <Box>
-        <Text
-          fontWeight="bold"
-          textAlign="justify"
-          fontSize="md"
-          color={textColor[colorMode]}
-          ml="4px"
-          mb="3px"
-        >
-          {/* Check out this */}
-          {summary}
-        </Text>
-      </Box>
-      <Box
-        display="flex"
-        alignItems="flex-start"
-        justifyContent="space-between"
-        flexDirection="column"
-      >
-        <Text
-          fontWeight="bold"
-          textAlign="justify"
-          fontSize="sm"
-          color="cyan.900"
-          ml="2px"
-          mb = "15px"
-        >
-          {reading_time["text"]}
-        </Text>
-        {/* <SimpleGrid
-          columns={4}
-          spacingX={2}
-          spacingY={2}
-          minChildWidth="80px"
-          mt="9px"
-          mb="10px"
-          minWidth="100%"
-          overflow="visible"
-        >
-          <ListTags tags={tags} />
-        </SimpleGrid> */}
-      </Box>
-      
-    </Container>
+    <RenderBlogCard
+      cover_image={cover_image}
+      summary={summary}
+      title={title}
+      slug={slug}
+      reading_time={reading_time}
+      tags={tags}
+    />
+    // <Container
+    //   mt="50px"
+    //   mb="15px"
+    //   // boxShadow="inset -30px -30px 29px #110e27,             inset 30px 30px 29px #151231;"
+    //   borderRadius="21px"
+    //   display="flex"
+    //   flexDirection="column"
+    //   alignItems="space-around"
+    //   justifyContent="center"
+    //   border={`2px solid ${borderColor[colorMode]}`}
+    //   minWidth="100%"
+    // >
+    //   <Box>
+    //     <Link href={`/blog/${slug}`}>
+    //       <a>
+    //         <Text
+    //           fontWeight="bold"
+    //           textAlign="justify"
+    //           color={textColor[colorMode]}
+    //           fontSize="xl"
+    //           letterSpacing="wide"
+    //           mt="15px"
+    //           mb="1px"
+    //           ml="4px"
+    //         >
+    //           {/* Title for the Blog */}
+    //           {title}
+    //         </Text>
+    //       </a>
+    //     </Link>
+    //   </Box>
+    //   <Box>
+    //     <Text
+    //       fontWeight="bold"
+    //       textAlign="justify"
+    //       fontSize="md"
+    //       color={textColor[colorMode]}
+    //       ml="4px"
+    //       mb="3px"
+    //     >
+    //       {/* Check out this */}
+    //       {summary}
+    //     </Text>
+    //   </Box>
+    //   <Box
+    //     display="flex"
+    //     alignItems="flex-start"
+    //     justifyContent="space-between"
+    //     flexDirection="column"
+    //   >
+    //     <Text
+    //       fontWeight="bold"
+    //       textAlign="justify"
+    //       fontSize="sm"
+    //       color="cyan.900"
+    //       ml="2px"
+    //       mb="15px"
+    //     >
+    //       {reading_time["text"]}
+    //     </Text>
+    //     {/* <SimpleGrid
+    //       columns={4}
+    //       spacingX={2}
+    //       spacingY={2}
+    //       minChildWidth="80px"
+    //       mt="9px"
+    //       mb="10px"
+    //       minWidth="100%"
+    //       overflow="visible"
+    //     >
+    //       <ListTags tags={tags} />
+    //     </SimpleGrid> */}
+    //   </Box>
+    // </Container>
   );
 };
 
